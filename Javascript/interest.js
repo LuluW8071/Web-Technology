450304e4fb8f12f6b15b7ea9cbc72a4a1d18dabe
luluw <shashinmhrzn@gmail.com>
2023-12-27 13:45:18 +0545

    constant variable

---
+++ Javascript/interest.js
@@ -0,0 +1,7 @@
+const interestRate = 0.3;
+// interestRate = 1;
+console.log(interestRate)
+
+// const means constatnt var meaning you are unable to change its value
+// so use let if value needs to be changed as per your need 
+// and use const if value needs to remain unchanged
