dd5aa5b585c60dd2624bec43ff214feaf9098d54
luluw <shashinmhrzn@gmail.com>
2023-12-27 15:03:20 +0545

    array and objects

---
+++ Javascript/03_array.js
@@ -0,0 +1,12 @@
+let selectedColor = ['red', 'green', 'blue'];
+console.log(selectedColor);
+
+// index []
+console.log(selectedColor[0]);
+
+// Insert new array element
+selectedColor [3] = 'cyan';
+console.log(selectedColor);
+
+// length
+console.log(selectedColor.length);
