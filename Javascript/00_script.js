bc28178feacc49f40ef58349c4270cf68413035c
luluw <shashinmhrzn@gmail.com>
2023-12-27 14:47:11 +0545

    Js variable declare & constants

---
+++ Javascript/00_script.js
@@ -0,0 +1,18 @@
+let name = 'luluw';
+console.log(name);
+
+// cannot be a reserved word
+// should be meaningful names
+// cannot start with a number
+// cannot contain a space or hyphen
+// are case insensitive
+
+let firstName = 'Happy', lastname = 'Goodbye';
+console.log(firstName, lastname);
+
+// Modern practice
+let var3 = 'Neutral';
+let var1 = 'Good';
+let var2 = 'Bad';
+
+console.log(var1, var2, var3);
