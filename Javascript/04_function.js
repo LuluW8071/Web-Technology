0f2cfcae7a2bb4b8868b4cf5ea046347a132b39b
luluw <shashinmhrzn@gmail.com>
2023-12-27 15:10:48 +0545

    function

---
+++ Javascript/04_function.js
@@ -0,0 +1,15 @@
+// Greet function
+function greet(name, lname) {
+    console.log('Hello ' + name + ' ' + lname);
+
+}
+
+// Calculate square of a number
+function square(n) {
+    return n * n;
+}
+
+// let n = square(2);
+console.log(square(2));
+
+greet('Programmer', 'Smith');
